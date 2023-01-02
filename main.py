f6fb18cdb695b4510a8e776f54482a748dce32df
777 <goszakupki.aktiv@gmail.com>
2023-01-02 06:16:36 +0300

    can send photo and mp3

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ def start(message):
     mess = f"Hello, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>"  # <u>подчеркнутый, андерлайн</u>  <b>жирный</b>
     bot.send_message(message.chat.id, mess, parse_mode="html")  #можно задавать формат в тегах, жирный шрифт
 
-@bot.message_handler()
+@bot.message_handler(content_types=["text"])
 def get_user_text(message):
-    bot.send_message(message.chat.id, message, parse_mode = "html")
+    if message.text == "Hello":
+        bot.send_message(message.chat.id, "And Hello to you", parse_mode = "html")
+    elif message.text == "id":
+        bot.send_message(message.chat.id, f"Your id is: {message.from_user.id}", parse_mode="html")
+    elif message.text == "photo":
+        photo = open("Kenguru2.png", "rb")
+        bot.send_photo(message.chat.id, photo)
+    elif message.text == "photo2":
+        photo2 = open("Kenguru.png", "rb")
+        bot.send_photo(message.chat.id, photo2)
+    elif message.text == "song1":
+        song1 = open("ivanushki-etazhi.mp3", "rb")
+        bot.send_audio(message.chat.id, song1)
+    else:
+        bot.send_message(message.chat.id, "I dont undestand you", parse_mode="html")
 
-
+# @bot.message_handler(content_types=["photo"])
+# def
 
 bot.polling(none_stop=True) #Чтоб бот работал постоянно
