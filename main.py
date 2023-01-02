14bd500d1ca3bf9ea5a410e132b629d812cdcb0c
777 <goszakupki.aktiv@gmail.com>
2023-01-02 05:30:28 +0300

    return {'content_type': 'text', 'id': 44, 'message_id': 44, 'from_user': {'id': 518239239, 'is_bot': False, 'first_name': 'Павел', 'username': 'Gos_oboronzakaz', 'last_name': None, 'language_code': 'ru', 'can_join_groups': None, 'can_read_all_group_messages': None, 'supports_inline_queries': None, 'is_premium': None, 'added_to_attachment_menu': None}, 'date': 1672626431, 'chat': {'id': 518239239, 'type': 'private', 'title': None, 'username': 'Gos_oboronzakaz', 'first_name': 'Павел', 'last_name': None, 'is_forum': None, 'photo': None, 'bio': None, 'join_to_send_messages': None, 'join_by_request': None, 'has_private_forwards': None, 'has_restricted_voice_and_video_messages': None, 'description': None, 'invite_link': None, 'pinned_message': None, 'permissions': None, 'slow_mode_delay': None, 'message_auto_delete_time': None, 'has_protected_content': None, 'sticker_set_name': None, 'can_set_sticker_set': None, 'linked_chat_id': None, 'location': None, 'active_usernames': None, 'emoji_status_custom_emoji_id': None}, 'sender_chat': None, 'forward_from': None, 'forward_from_chat': None, 'forward_from_message_id': None, 'forward_signature': None, 'forward_sender_name': None, 'forward_date': None, 'is_automatic_forward': None, 'reply_to_message': None, 'via_bot': None, 'edit_date': None, 'has_protected_content': None, 'media_group_id': None, 'author_signature': None, 'text': 'gg', 'entities': None, 'caption_entities': None, 'audio': None, 'document': None, 'photo': None, 'sticker': None, 'video': None, 'video_note': None, 'voice': None, 'caption': None, 'contact': None, 'location': None, 'venue': None, 'animation': None, 'dice': None, 'new_chat_member': None, 'new_chat_members': None, 'left_chat_member': None, 'new_chat_title': None, 'new_chat_photo': None, 'delete_chat_photo': None, 'group_chat_created': None, 'supergroup_chat_created': None, 'channel_chat_created': None, 'migrate_to_chat_id': None, 'migrate_from_chat_id': None, 'pinned_message': None, 'invoice': None, 'successful_payment': None, 'connected_website': None, 'reply_markup': None, 'message_thread_id': None, 'is_topic_message': None, 'forum_topic_created': None, 'forum_topic_closed': None, 'forum_topic_reopened': None, 'json': {'message_id': 44, 'from': {'id': 518239239, 'is_bot': False, 'first_name': 'Павел', 'username': 'Gos_oboronzakaz', 'language_code': 'ru'}, 'chat': {'id': 518239239, 'first_name': 'Павел', 'username': 'Gos_oboronzakaz', 'type': 'private'}, 'date': 1672626431, 'text': 'gg'}}

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Test!!!")
+import telebot
+
+bot = telebot.TeleBot("5930354248:AAGtL1XeJTu0P84wz-FMHZAWRKiFhajlHwk");
+
+@bot.message_handler(commands = ["start"])
+def start(message):
+    mess = f"Hello, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>"  # <u>подчеркнутый, андерлайн</u>  <b>жирный</b>
+    bot.send_message(message.chat.id, mess, parse_mode="html")  #можно задавать формат в тегах, жирный шрифт
+
+@bot.message_handler()
+def get_user_text(message):
+    bot.send_message(message.chat.id, message, parse_mode = "html")
+
+
+
+bot.polling(none_stop=True) #Чтоб бот работал постоянно
